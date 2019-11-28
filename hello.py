6db869d31cec8a21fa89ff455481897c3586c4f3
Harshil Darji <harshildarji2001@gmail.com>
2019-11-28 18:55:19 +0530

    added a line
    to check execution is starting

--- hello.py
+++ hello.py
@@ -7,5 +7,6 @@ def hello():
 	return "Hello World!"
 
 if __name__ == "__main__":
+	print("Program started...")
 	port = int(os.environ.get("PORT", 5000))
 	app.run(debug=True,host='0.0.0.0',port=port)
