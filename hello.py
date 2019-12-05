b7f01c0676d99284a1794436ddda3a52f0b1a271
Harshil Darji <harshildarji2001@gmail.com>
2019-12-06 00:58:41 +0530

    updated text

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	return "Hello World! I am Harshil Darji. Welcome to my mini-project site! :D"
 
 if __name__ == "__main__":
 	print("Program started...")
